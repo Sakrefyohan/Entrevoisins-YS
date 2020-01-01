32c283449c736a25de8600947003561f998ffc5b
Sakrefyohan <yohan.sakref@gmail.com>
2020-01-01 21:16:16 +0100

    Bindag du bouton Fav

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/users_details/UsersDetailsActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/users_details/UsersDetailsActivity.java
@@ -13,26 +13,33 @@ import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourPagerAdapt
 
 import org.w3c.dom.Text;
 
+import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class UsersDetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private final static int RETURNBUTTON = 1;
-
-    private ImageView mInfo;     //neighbours_info_picture;
-    private Button mFavButton;        //neighbours_info_fav_button;
-    private TextView mNameSmall;      //neighbours_info_name_small;
-    private TextView mAddress;      //neighbours_info_address;
-    private TextView mNumber;      //neighbours_info_number;
-    private TextView mLink;      //neighbours_info_link;
-    private TextView mAproposTitle;      //neighbours_info_apropos_title;
-    private TextView mAproposText;      //neighbours_info_apropos_text;
-    private TextView mNameBig;      //neighbours_info_name_big;
-    private TextView mReturnButton;      //neighbours_info_return_button;
+   // private ImageView mInfo;          //neighbours_info_picture;
+   // private Button mFavButton;        //neighbours_info_fav_button;
+   // private TextView mNameSmall;      //neighbours_info_name_small;
+   // private TextView mAddress;        //neighbours_info_address;
+   // private TextView mNumber;         //neighbours_info_number;
+   // private TextView mLink;           //neighbours_info_link;
+   // private TextView mAproposTitle;   //neighbours_info_apropos_title;
+   // private TextView mAproposText;    //neighbours_info_apropos_text;
+   // private TextView mNameBig;        //neighbours_info_name_big;
 
 
-
-
+    @BindView(R.id.neighbours_info_picture) ImageView mPicture;
+    @BindView(R.id.neighbours_info_fav_button)Button mFavButton;
+    @BindView(R.id.neighbours_info_name_small)TextView mNameSmall;
+    @BindView(R.id.neighbours_info_address) TextView mAddress;
+    @BindView(R.id.neighbours_info_number) TextView mNumber;
+    @BindView(R.id.neighbours_info_link) TextView mLink;
+    @BindView(R.id.neighbours_info_apropos_title) TextView mAproposTitle;
+    @BindView(R.id.neighbours_info_apropos_text)TextView mAproposText;
+    @BindView(R.id.neighbours_info_name_big)TextView mNameBig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,54 +47,18 @@ public class UsersDetailsActivity extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_users_details);
         ButterKnife.bind(this);
 
-        mInfo = findViewById(R.id.neighbours_info_picture);
-        mFavButton = findViewById(R.id.neighbours_info_fav_button);
-        mNameSmall = findViewById(R.id.neighbours_info_name_small);
-        mAddress = findViewById(R.id.neighbours_info_address);
-        mNumber = findViewById(R.id.neighbours_info_number);
-        mLink = findViewById(R.id.neighbours_info_link);
-        mAproposTitle = findViewById(R.id.neighbours_info_apropos_title);
-        mAproposText = findViewById(R.id.neighbours_info_apropos_text);
-        mNameBig = findViewById(R.id.neighbours_info_name_big);
+    }
 
+    // Clique sur le bouton Fav
+    // Modification de l'etat du voisin
+    @OnClick(R.id.neighbours_info_fav_button)
+    void setFavButton(View view){
 
-
-        // Clique sur les boutons
-        mReturnButton.setOnClickListener(this);
-        mFavButton.setOnClickListener(this);
-
-        // Tag des boutons
-        mReturnButton.setTag(1);
-        mFavButton.setTag(2);
-
+        if(neighbourIsFav) {
+            neighbourIsFav = False;
+        }else {neighbourIsFav = True;}
     }
 
 
-    @Override
-    public void onClick(View v) {
-        int responseIndex = (int) v.getTag();
-
-        switch (responseIndex){
-            case 1:
-                //retour a la liste des voisins
-                break;
-            case 2:
-                // Mettre/enlever le voisin en favoris
-
-
-               // Controle : si le voisin est déjà en favoris alors on le retire, sinon on l'ajout en favoris
-               //
-               //if (laVariableDuVoisin == true){
-               //  laVariableDuVoisin = False;
-               //  [On change la couleur du bouton fav]
-               // }Else {
-               // laVariableDuVoisin = True;
-               //  }
-               //
-                break;
-        }
-
-
-    }
 }
 
